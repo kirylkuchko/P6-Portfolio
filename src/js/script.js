c64a6fb6b8be0f5e4a8059ea7d57318d95a8f98c
Kiryl Kuchko <kuchkokirill@mail.ru>
2022-04-13 10:51:48 +0200

    Added script for auto bar filling

--- src/js/script.js
+++ src/js/script.js
@@ -9,3 +9,10 @@ hamburger.addEventListener('click', ()=>{
 closeElem.addEventListener('click', ()=>{
     menu.classList.remove('active');
 });
+
+const counters = document.querySelectorAll('.skills__percentage-value'),
+    lines = document.querySelectorAll('.skills__percentage-bar-filling');
+
+counters.forEach( (item, i) => {
+    lines[i].style.width = item.innerHTML;
+});
